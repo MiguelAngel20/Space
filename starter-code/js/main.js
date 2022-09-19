495f6f0da4493b1b99a97652c339abecf4656392
Virbes <francisco.virbes.457@gmail.com>
2022-09-19 03:39:13 -0500

    Terminando botones de navegacion

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,7 +1,33 @@
-const ruta = window.location.href;
+const ruta = window.location.pathname;
 
-if (ruta === 'http://127.0.0.1:5500/starter-code/destination-moon.html') {
-    const body = document.getElementById('body');
+const body = document.querySelector('body');
 
-    body.classList.add('background-destination');
+const home = '/starter-code/index.html';
+const destination = '/starter-code/destination-moon.html';
+const crew = '/starter-code/crew';
+const technology = '/starter-code/technology.html';
+
+switch (ruta) {
+    case '/starter-code/':
+        body.classList.add('background-home');
+        document.querySelector('.menu_home').classList.add('active');
+        break;
+    case home:
+        body.classList.add('background-home');
+        document.querySelector('.menu_home').classList.add('active');
+        break;
+    case destination:
+        body.classList.add('background-destination');
+        document.querySelector('.menu_destination').classList.add('active');
+        break;
+    case crew:
+        body.classList.add('background-crew');
+        document.querySelector('.menu_crew').classList.add('active');
+        break;
+    case technology:
+        body.classList.add('background-technology');
+        document.querySelector('.menu_technology').classList.add('active');
+        break;
 }
+
+localStorage.setItem('ruta', ruta);
