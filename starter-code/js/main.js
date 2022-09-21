a290ab4e03f9c0658a61079455b2974e193b12d4
MiguelAngel20 <mangel20sr@gmail.com>
2022-09-21 01:11:14 -0500

    Crew 1.0v

---
+++ starter-code/js/main.js
@@ -0,0 +1,31 @@
+const ruta = window.location.pathname;
+
+const body = document.querySelector('body');
+
+const home = '/starter-code/index.html';
+const destination = '/starter-code/destination-moon.html';
+const crew = '/starter-code/crew';
+const technology = '/starter-code/technology-vehicle.html';
+
+switch (ruta) {
+    case '/starter-code/':
+        body.classList.add('background-home');
+        document.querySelector('.menu_home').classList.add('active');
+        break;
+    case home:
+        body.classList.add('background-home');
+        document.querySelector('.menu_home').classList.add('active');
+        break;
+    case destination:
+        body.classList.add('background-destination');
+        document.querySelector('.menu_destination').classList.add('active');
+        break;
+    case crew:
+        body.classList.add('background-crew');
+        document.querySelector('.menu_crew').classList.add('active');
+        break;
+    case technology:
+        body.classList.add('background-technology');
+        document.querySelector('.menu_technology').classList.add('active');
+        break;
+}
