89e5d939635f2f3dca3fa31ecbd9c429d834e5e0
Mario <tomasdevs@gmail.com>
2022-09-26 21:35:40 -0500

    ayuda2>

---
+++ starter-code/js/destination.js
@@ -0,0 +1,17 @@
+import data from '../data.json' assert {type: 'json'};
+
+let moonBtn = document.querySelector('#moon');
+let marsBtn = document.querySelector('#mars');
+let europaBtn = document.querySelector('#europa');
+let titanBtn = document.querySelector('#titan');
+
+let imagen = document.querySelector('#images');
+let titulo = document.querySelector('#name');
+let descripcion = document.querySelector('#description');
+
+titulo.innerHTML = 
+
+moonBtn.addEventListener('click', ()=>{
+        
+});
+console.log(data);
